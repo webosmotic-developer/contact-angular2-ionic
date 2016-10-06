801484f86a89a3ab7a11fc1b7ceb80254af3aca1
sweety-wo <sweety.webashlar@gmail.com>
2016-10-06 11:43:40 +0530

    Form validation.

--- src/pages/contact.details/contact.details.ts
+++ src/pages/contact.details/contact.details.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController, NavParams, AlertController, LoadingController } from 'ionic-angular';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 import { ContactPage } from "../contact/contact";
 import { Contact } from "../../shared/contact/contact";
@@ -16,10 +17,39 @@ export class ContactDetailsPage{
 
     loader;
     contact:Contact;
+    form: FormGroup;
+    submitAttempt: boolean = false;
 
     constructor(public nav:NavController, public params:NavParams, public loading: LoadingController,
-                public alert: AlertController, private contactService:ContactService) {
+                public alert: AlertController, public fbld: FormBuilder, private contactService:ContactService) {
         this.contact = this.params.get('contact') ? this.params.get('contact') : new Contact();
+
+        this.form = this.fbld.group({
+            name: ['',[Validators.required]],
+            email:['',[Validators.compose([Validators.required])]],
+            phone:['',[Validators.required]]
+        });
+
+        if(this.params.get('contact')){
+            (<FormControl>this.form.controls['name'])
+                .setValue(this.contact.name, { onlySelf: true });
+
+            (<FormControl>this.form.controls['email'])
+                .setValue(this.contact.email, { onlySelf: true });
+
+            (<FormControl>this.form.controls['phone'])
+                .setValue(this.contact.phone, { onlySelf: true });
+        }
+    }
+
+    fnOnSubmit() {
+        this.submitAttempt = true;
+        if(this.form.valid){
+            this.contact.name = this.form.value.name;
+            this.contact.email= this.form.value.email;
+            this.contact.phone= this.form.value.phone;
+            this.fnSaveContact();
+        }
     }
 
     fnLoading() {
@@ -30,48 +60,27 @@ export class ContactDetailsPage{
         this.loader.present();
     }
 
-    fnValidateEmail(email:string) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(email);
-    }
-
-    fnValidatePhone(phone:string) {
-        var re = /^\d{10}$/;
-        return re.test(phone);
-    }
 
     fnSaveContact() {
-        if (!this.contact.name) {
-            alert("Name is required.");
-        } else if (!this.contact.email) {
-            alert("Email is required.");
-        } else if (this.contact.email && !this.fnValidateEmail(this.contact.email)) {
-            alert("Enter valid email.");
-        } else if (!this.contact.phone) {
-            alert("Phone is required with numeric value.");
-        } else if (this.contact.phone && !this.fnValidatePhone(this.contact.phone)) {
-            alert("Enter valid 10 digits number.");
+        this.fnLoading();
+        if (!this.contact._id) {
+            this.contactService.fnAddContact(this.contact)
+                .subscribe(() => {
+                    this.contact = new Contact();
+                    this.loader.dismissAll();
+                    this.nav.push(ContactPage);
+                },() => {
+                    this.loader.dismissAll();
+                });
         } else {
-            this.fnLoading();
-            if (!this.contact._id) {
-                this.contactService.fnAddContact(this.contact)
-                    .subscribe(() => {
-                        this.contact = new Contact();
-                        this.loader.dismissAll();
-                        this.nav.push(ContactPage);
-                    },() => {
-                        this.loader.dismissAll();
-                    });
-            } else {
-                this.contactService.fnUpdateContact(this.contact)
-                    .subscribe(() => {
-                        this.contact = new Contact();
-                        this.loader.dismissAll();
-                        this.nav.push(ContactPage);
-                    },() => {
-                        this.loader.dismissAll();
-                    });
-            }
+            this.contactService.fnUpdateContact(this.contact)
+                .subscribe(() => {
+                    this.contact = new Contact();
+                    this.loader.dismissAll();
+                    this.nav.push(ContactPage);
+                },() => {
+                    this.loader.dismissAll();
+                });
         }
     }
 
